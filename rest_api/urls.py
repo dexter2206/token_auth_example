517ab69f990ee57495136b0e649fcf58dead2214
dexter2206 <dexter2206@gmail.com>
2020-12-06 08:43:37 +0100

    Implement REST endpoints for managing Notes

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import HelloView
+from .views import HelloView, NotesView
 
 
 urlpatterns = [
     path("auth", obtain_auth_token),
-    path("hello-world", HelloView().as_view())
+    path("hello-world", HelloView().as_view()),
+    path("notes", NotesView().as_view())
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,8 +1,29 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from notes.models import Note
+from rest_api.serializers import NoteSerializer
 
 
 class HelloView(APIView):
     def get(self, request):
         content = {"message": "Hello World"}
         return Response(content)
+
+
+class NotesView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user_notes = Note.objects.filter(owner=request.user)
+        return Response(NoteSerializer(user_notes, many=True).data)
+
+    def post(self, request):
+        serializer = NoteSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save(owner=request.user)
+
+        return Response(serializer.data)
+
+
