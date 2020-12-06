6974c3dba400cc7dd5c28a6ebb877c608910f53b
dexter2206 <dexter2206@gmail.com>
2020-12-06 07:32:39 +0100

    Add hello-world view

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import HelloView
 
 
 urlpatterns = [
-    path("auth", obtain_auth_token)
+    path("auth", obtain_auth_token),
+    path("hello-world", HelloView().as_view())
 ]
