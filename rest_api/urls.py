cb71ba13ca7347124f70f493f526ce3f073ccef7
dexter2206 <dexter2206@gmail.com>
2020-12-16 10:05:31 +0100

    Add JWT support

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .views import HelloView, NotesView
 
 
 urlpatterns = [
     path("auth", obtain_auth_token),
     path("hello-world", HelloView().as_view()),
-    path("notes", NotesView().as_view())
+    path("notes", NotesView().as_view()),
+    path('jwt/auth', TokenObtainPairView.as_view()),
+    path('jwt/refresh', TokenRefreshView.as_view()),
 ]
